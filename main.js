9379ad7a80a0ca6b6e1eb72ae0f9f18fafaf68ad
ngocgia <vodangngocgia@gmail.com>
2023-03-17 22:30:50 +0700

    test

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.get('/home', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "home.html"));
+})
 
 app.listen(port, () => console.log('App running port 3000!'));
