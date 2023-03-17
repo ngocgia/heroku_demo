e2d26b53f01ef411abddd6cce4821645cce5d946
ngocgia <vodangngocgia@gmail.com>
2023-03-17 23:51:34 +0700

    init template

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+// const port = process.env.PORT; // cấu hình heroku
+const port = 3000;
+const path = require("path");
 
-app.get('/home', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "home.html"));
-})
+app.use(express.static('pages'))
+
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'pages/index.html')));
 
 app.listen(port, () => console.log('App running port 3000!'));
